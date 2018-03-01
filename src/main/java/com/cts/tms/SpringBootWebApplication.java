7420f225cfb34f30bd75be261cffb9b624f9657a
Mukesh Kumar <mukesh.j.dangi@gmail.com>
2018-03-01 18:21:47 +0530

    updating app

--- src/main/java/com/cts/tms/SpringBootWebApplication.java
+++ src/main/java/com/cts/tms/SpringBootWebApplication.java
@@ -4,31 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication
-@ComponentScan({ "com.cts.tms" })
-@PropertySource("classpath:application.properties")
 public class SpringBootWebApplication extends SpringBootServletInitializer {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SpringBootWebApplication.class);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootWebApplication.class);
+    }
 
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(SpringBootWebApplication.class, args);
-	}
-	
-	@Bean
-    public InternalResourceViewResolver jspViewResolver() {
-        InternalResourceViewResolver bean = new InternalResourceViewResolver();
-        bean.setPrefix("/WEB-INF/views/");
-        bean.setSuffix(".jsp");
-        return bean;
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SpringBootWebApplication.class, args);
     }
 
 }
